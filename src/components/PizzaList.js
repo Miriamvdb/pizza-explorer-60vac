98e5f448ab03011e7c9ac597718ebf46f8dad1eb
Miriamvdb <miriamvdb@live.nl>
2022-08-05 16:54:25 +0200

    SCSS styling

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux"; // 3.
+import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../store/user/selectors";
 import {
   selectAllPizzas,
@@ -7,50 +7,64 @@ import {
 import AddNewPizza from "./AddNewPizza";
 import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
 import { toggleFav } from "../store/user/slice";
+import "./PizzaList.scss";
 
 const PizzaList = () => {
   const user = useSelector(selectUser);
   const amountOfPizzas = useSelector(selectAmountOfPizzas);
   const allPizzas = useSelector(selectAllPizzas);
-  const dispatch = useDispatch(); // 3.
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Pizza Explorer</h1>
-      <p>
+    <div className="pizza-list">
+      <h1 style={{ fontSize: "50px" }}>Pizza Explorer</h1>
+      <p style={{ fontSize: "15px" }}>
         Welcome back, <strong>{user.name}</strong>!
       </p>
+      <br />
       <h2>Pizza's ({amountOfPizzas})</h2>
-      {allPizzas.map((pizza, index) => {
-        return (
-          <div key={index}>
-            <h3>{pizza.name}</h3>
-            {/* 1. Add a heart next to each pizza (fav or not) */}
-
-            {/* 4. Dispatch it from the heart-button, we'll pass in */}
-            {/* the id of the current pizza as a parameter so we can */}
-            {/* add it as a favorite next! */}
-            <button onClick={() => dispatch(toggleFav(pizza.id))}>
-              {user.favorites.includes(pizza.id) ? (
-                <BsSuitHeartFill />
-              ) : (
-                <BsSuitHeart />
-              )}
-            </button>
-
-            <p>
-              <b>Description: </b>
-              {pizza.description}
-            </p>
-            <p>
-              <b>Bought: </b>
-              {pizza.bought}
-            </p>
-            <img src={pizza.image} alt={pizza.name} />
-          </div>
-        );
-      })}
+      <div className="pizzas">
+        {allPizzas.map((pizza, index) => {
+          return (
+            <div key={index}>
+              <div className="fav-and-title">
+                <button
+                  onClick={() => dispatch(toggleFav(pizza.id))}
+                  className="fav-toggle"
+                >
+                  {user.favorites.includes(pizza.id) ? (
+                    <BsSuitHeartFill
+                      style={{ fontSize: "15px", color: "tomato" }}
+                    />
+                  ) : (
+                    <BsSuitHeart
+                      style={{ fontSize: "15px", color: "tomato" }}
+                    />
+                  )}
+                </button>
+                <h3>{pizza.name}</h3>
+              </div>
+              <div
+                className="pizza"
+                style={{ backgroundImage: `url(${pizza.image})` }}
+              >
+                <div className="overlay">
+                  <p>
+                    <b>Description: </b>
+                    {pizza.description}
+                  </p>
+                  <p>
+                    <b>Bought: </b>
+                    {pizza.bought}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <div>
+        <br />
         <AddNewPizza />
       </div>
     </div>
